926a674c075bafe179f4f0b2106de51d3c8bfac0
AbdelqaderAlOma <>
2021-05-06 12:10:13 +0000

    Updated app.js for lab 05

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-confirm("ready to get started?")
+/*confirm("ready to get started?")
 var websitePrice = 300
 var userWeb = prompt('Whats your favourite website; business, store, blog or personal?')
 if(userWeb === 'personal'){
@@ -11,4 +11,29 @@ if(userWeb === 'personal'){
   document.write('The Website Price is: ' + websitePrice * 2.5 + '$')
 }else {
   alert('You should choose a type of website you want')
+}*/
+var userName = prompt('enter your username')
+var userPass = prompt('enter your password')
+while(userPass !== "0000") {
+userPass = prompt('Invalid password, please enter the correct one')
 }
+var imgCount = prompt('What the number of websites do you want?')
+var webType = prompt('What type of website do you prefer?Personal/Blog/Business/Store')
+var image2 = ''
+for(var x= 0; x < imgCount; x++){
+  if(webType === 'Business'){
+    image2 = image2 + '<img src="https://steemitimages.com/DQmSr9mGvK5FZdEb5koVXiJJ5e1nTmBqPXrDE46jg1smUXA/Business%20website.jpg">'
+  }else if(webType ==='Blog'){
+    image2 = image2 + '<img src="https://i.pinimg.com/originals/06/8f/c6/068fc6b38c5b723c92b1688c1bc65362.jpg">'
+  }else if(webType ==='Store'){
+    image2 = image2 + '<img src="https://cdn.dribbble.com/users/961146/screenshots/10904922/shot_4x.jpg">'
+  }else if(webType ==='Personal'){
+    image2 = image2 + '<img src="https://colorlib.com/wp/wp-content/uploads/sites/2/personal-free-template.jpg">'}
+}
+  document.write(image2)
+var starNum = prompt('What star rating would you give, 1-5?')
+var image = ''
+for(var i = 0; i < starNum ;i++){
+    image = image + '<img src="http://icons.iconarchive.com/icons/cheezen/web-2/128/star-icon.png">'
+}
+  document.write(image)
